197e1ab2734f91008d5970a991b824e978f8d5cd
Philippe Nougaillon <philippe.nougaillon@gmail.com>
2021-10-15 11:55:54 +0200

    code refacto

---
+++ app/javascript/bundles/Events/components/PageEvents.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import Search from "./Search"
+import Events from "./Events"
+
+const PageEvents = (props) => {
+
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'      
+  );
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    localStorage.setItem('search', event.target.value);
+    console.log(event.target.value);
+  };
+
+  const searchedEvents = props.items.filter((item) => 
+    item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  ); 
+
+  return (
+    <React.Fragment>
+      <h1>{ props.title }</h1>
+
+      <Search search={ searchTerm } onSearch={ handleChange }>
+        Rechercher :
+      </Search>
+
+      { searchTerm && 
+        <p>Searching for <strong>{ searchTerm }</strong>...</p>
+      }
+
+      <Events items={ searchedEvents } />
+    </React.Fragment>
+  );
+}
+
+PageEvents.propTypes = {
+  title: PropTypes.string,
+  items: PropTypes.array
+};
+
+export default PageEvents
